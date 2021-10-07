f4fe836b958128d5cedbd69f6849d442f7ed4fe3
mattndross <mattndross@users.noreply.github.com>
2021-10-07 20:18:39 +0200

    update CardGenerator component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Col, Row, Card, Button, Form } from "react-bootstrap";
 import CardGenerator from "./components/CardGenerator";
+
+
+const quoteObject = [
+  {
+    quote:
+      "It’s not the years in your life that count. It’s the life in your years.",
+    author: "Abraham Lincoln",
+  },
+  {
+    quote:
+      "It’s not the years in your life that count. It’s the life in your years.",
+    author: "Abraham Lincoln",
+  },
+  {
+    quote:
+      "It’s not the years in your life that count. It’s the life in your years.",
+    author: "Abraham Lincoln",
+  }
+];
+
 function App() {
+
+  
   return (
     <div className="App">
       <Container>
@@ -34,7 +56,7 @@ function App() {
           </Form.Group>
         </Form>
         <Container className="card-container mt-4">
-          <CardGenerator />
+          <CardGenerator quoting={quoteObject}/>
         </Container>
       </Container>
     </div>
--- src/components/CardGenerator.js
+++ src/components/CardGenerator.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
+import React from "react";
+import Card from "react-bootstrap/Card";
 
-const quoteObj = {
-    quote:
-      "It’s not the years in your life that count. It’s the life in your years.",
-    author: "Abraham Lincoln",
-  };
+const renderCard = (quoteObj) => {
+  return (
+    <Card className="mb-3">
+      <Card.Body>
+        <Card.Text>{quoteObj.quote}</Card.Text>
+        <Card.Subtitle className="text-muted">-{quoteObj.author}</Card.Subtitle>
+      </Card.Body>
+    </Card>
+  );
+};
 
-
-export default function CardGenerator() {
-    return (
-        <Card className="mb-3">
-            <Card.Body>
-              <Card.Text>{quoteObj.quote}</Card.Text>
-              <Card.Subtitle className="text-muted">-{quoteObj.author}</Card.Subtitle>
-            </Card.Body>
-          </Card>
-    )
+export default function CardGenerator({quoting}) {
+    if (quoting && Array.isArray(quoting)) {
+       return quoting.map((quoteObj) => {
+         return renderCard(quoteObj);
+       });
+     } else {
+       return renderCard(quoting);
+     }  
 }
+
